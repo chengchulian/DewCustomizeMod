96f24e4acd958a08652a87a20af19f2d2c06b8b3
chengchulian <chengchulian@qq.com>
2025-06-01 10:07:31 +0800

    使客户端同步槽数

---
+++ DewCustomizeMod/patch/UI_Common_ChatBoxPatch.cs
@@ -0,0 +1,16 @@
+﻿using DewCustomizeMod.util;
+using HarmonyLib;
+
+namespace DewCustomizeMod.patch;
+
+[HarmonyPatch(typeof(UI_Common_ChatBox))]
+public class UI_Common_ChatBoxPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch("ClientEventOnMessageReceived")]
+    public static bool ClientEventOnMessageReceivedPrefix(UI_Common_ChatBox __instance, ChatManager.Message obj)
+    {
+        GameManagerUtil.ClientSyncData(obj);
+        return true;
+    }
+}
